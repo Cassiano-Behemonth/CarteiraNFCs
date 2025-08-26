98792a58c1b0c06f33a6418d7f233d4e1ebf1ad6
Cassiano-Behemonth <oliveira.cassiano@escola.pr.gov.br>
2025-08-26 02:42:16 -0300

    Imports e adição de icones

--- app/src/main/java/com/example/carteiranfcs/MainActivity.kt
+++ app/src/main/java/com/example/carteiranfcs/MainActivity.kt
@@ -3,27 +3,36 @@ package com.example.carteiranfcs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.carteiranfcs.ui.theme.CarteiraNFCsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CarteiraNFCsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White
+                ) {
+                    CarteiraScreen()
                 }
             }
         }
@@ -31,17 +40,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CarteiraScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 50.dp, start = 16.dp, end = 16.dp)
+    ) {
+
+        AvatarBadge(modifier = Modifier.align(Alignment.TopEnd))
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 90.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_nfc),
+                contentDescription = "NFC",
+                modifier = Modifier.size(150.dp)
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+
+            Box(
+                modifier = Modifier
+                    .size(width = 300.dp, height = 180.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color(0xFF9C295B))
+            ) {
+                TextosCartao()
+            }
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Row {
+                repeat(5) { index ->
+                    Box(
+                        modifier = Modifier
+                            .size(20.dp)
+                            .padding(4.dp)
+                            .clip(CircleShape)
+                            .background(if (index == 0) Color.Blue else Color.Gray)
+                    )
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CarteiraNFCsTheme {
-        Greeting("Android")
-    }
 }
